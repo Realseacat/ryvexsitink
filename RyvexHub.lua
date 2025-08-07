0411d8df18e31a21b46f43fb0ab2c7d9a809a5ec
Realseacat <vvn3353@gmail.com>
2025-08-07 08:56:46 +0700

    Update RyvexHub.lua

--- RyvexHub.lua
+++ RyvexHub.lua
@@ -1,6 +1,6 @@
 local UniverseID = game:GetService("HttpService"):JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/"..game.PlaceId.."/universe")).universeId
 if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BF-BananaCat.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AdministratorSeaCat/ryvexsitick/refs/heads/main/RyvexHub.lua"))()
 elseif game.PlaceId == 6839171747 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Realseacat/RealSeaCat/refs/heads/main/Door.lua"))()
 end
