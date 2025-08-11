359d7c595ad427d01775cbfc9d3298c5524461b5
Realseacat <vvn3353@gmail.com>
2025-08-11 18:43:11 +0700

    Update RyvexHub.lua

--- RyvexHub.lua
+++ RyvexHub.lua
@@ -4,10 +4,13 @@ local UniverseID = game:GetService("HttpService"):JSONDecode(
 
 if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AdministratorSeaCat/ryvexsitick/refs/heads/main/RyvexHub.lua"))()
+
 elseif game.PlaceId == 6839171747 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Realseacat/RealSeaCat/refs/heads/main/Door.lua"))()
+
 elseif game.PlaceId == 79546208627805 or game.PlaceId == 126509999114328 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Realseacat/RealSeaCat/refs/heads/main/Ryvex99nights%20intheforest.lua"))()
-elseif game.PlaceId == 11575526212 then
+
+elseif game.PlaceId == 126884695634066 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Realseacat/RealSeaCat/refs/heads/main/RYVEX4.lua"))()
 end
