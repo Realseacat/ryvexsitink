fb6c3adbe66360981fff8f69073f75f6393b5ddf
Realseacat <vvn3353@gmail.com>
2025-08-12 09:07:25 +0700

    Update RyvexHub.lua

--- RyvexHub.lua
+++ RyvexHub.lua
@@ -3,8 +3,7 @@ local UniverseID = game:GetService("HttpService"):JSONDecode(
 ).universeId
 
 if game.PlaceId == 7449423635 or game.PlaceId == 2753915549 or game.PlaceId == 4442272183 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/AdministratorSeaCat/ryvexsitick/refs/heads/main/RyvexHub.lua"))()
-
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Realseacat/BloxFuit/refs/heads/main/RyvexHub.lua"))()
 elseif game.PlaceId == 6839171747 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Realseacat/RealSeaCat/refs/heads/main/Door.lua"))()
 
